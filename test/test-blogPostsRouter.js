2cb33c0140921d3e4c275276fbef3d03c84c3e60
Rebecca Bob <rebecca.rupel@gmail.com>
2019-04-08 21:58:05 -0700

    added mocha and tests

--- test/test-blogPostsRouter.js
+++
@@ -1,8 +0,0 @@
-const chai = require("chai");
-const chaiHttp = require("chai-http");
-
-const expect = chai.expect;
-
-const { app, runServer, closeServer } = require("../server");
-
-chai.use(chaiHttp);
