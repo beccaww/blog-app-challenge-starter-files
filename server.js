2026d4bcbb2a3cb4a5699480c714e4eb55851ce3
Rebecca Bob <rebecca.rupel@gmail.com>
2019-04-10 13:44:45 -0700

    added server functions

--- server.js
+++ server.js
@@ -1,18 +1,48 @@
 const express = require("express");
 const morgan = require("morgan");
 
-const bprouter = require("./blogPostsRouter");
-
+const blogPostsRouter = require("./blogPostsRouter");
 const app = express();
 
 app.use(morgan("common"));
 app.use(express.json());
 
-// you need to import `blogPostsRouter` router and route
+app.use("/blog-posts", blogPostsRouter);
 
-// requests to HTTP requests to `/blog-posts` to `blogPostsRouter`
-app.use('/blog-posts', bprouter);
 
-app.listen(process.env.PORT || 8080, () => {
-  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
-});
+let server;
+
+function runServer() {
+  const port = process.env.PORT || 8080;
+  return new Promise((resolve, reject) => {
+    server = app
+      .listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve(server);
+      })
+      .on("error", err => {
+        reject(err);
+      });
+  });
+}
+
+
+function closeServer() {
+  return new Promise((resolve, reject) => {
+    console.log("Closing server");
+    server.close(err => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve();
+    });
+  });
+}
+
+
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+}
+
+module.exports = { app, runServer, closeServer };
